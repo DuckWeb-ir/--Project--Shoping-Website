65e68c7ed15689f8037dccb9d8e08a94dd2ad6b1
DuckWeb-ir <ahmadkhamisabadi81@gmail.com>
2026-08-28 21:39:07 +0330

    use React-Select library for seller name field

--- src/Components/Templates/Dashboard/common/ProductDrawer/SellerField.jsx
+++ src/Components/Templates/Dashboard/common/ProductDrawer/SellerField.jsx
@@ -1,64 +1,77 @@
-import React from 'react'
 import { BiPlus } from "react-icons/bi";
 import { HiX } from "react-icons/hi";
 import AsyncSelect from "react-select/async";
+import { searchSellers } from "../../../../../services/seller.service";
 
 const SellerFields = ({ sellers, onAdd, onChange, onRemove }) => {
+    const loadSellersOptions = async (inputValue) => {
+        const sellers = await searchSellers(inputValue);
+
+        return sellers.map((seller) => ({
+            value: seller._id,
+            label: seller.name,
+        }));
+    };
 
     return (
         <div>
             <div className="flex items-center justify-between mb-2">
-                <label className="text-sm font-medium text-zinc-700">
-                    فروشندگان
-                </label>
-
+                <label className="text-sm font-medium text-zinc-700">فروشندگان</label>
                 <button
                     type="button"
-                    className="text-blue-500 text-xs flex items-center gap-1"
                     onClick={onAdd}
+                    className="text-blue-500 text-xs flex items-center gap-1"
                 >
-                    <BiPlus />
-                    افزودن فروشنده
+                    <BiPlus /> افزودن فروشنده
                 </button>
             </div>
 
             {sellers.map((seller, index) => (
                 <div key={index} className="flex items-center gap-2">
+                    <div>
                         <AsyncSelect
+                            cacheOptions
+                            defaultOptions
+                            loadOptions={loadSellersOptions}
+                            value={seller.info}
+                            onChange={(option) => {
+                                onChange(index, "info", option);
+                                onChange(index, "id", option);
+                            }}
                             loadingMessage={() => "در حال جستجو ..."}
                             noOptionsMessage={() => "فروشنده‌ای یافت نشد"}
                             placeholder="جستجوی فروشنده"
                         />
-                    
-                        <input
-                            value={seller.price}
-                            onChange={(e) => onChange(index, "price", e.target.value)}
-                            placeholder="قیمت"
-                            type="number"
-                            className="w-1/4 h-9 text-sm rounded-md outline-none primary-border px-2"
-                        />
+                    </div>
 
-                        <input
-                            value={seller.stock}
-                            onChange={(e) => onChange(index, "stock", e.target.value)}
-                            placeholder="موجودی"
-                            type="number"
-                            className="w-1/4 h-9 text-sm rounded-md outline-none primary-border px-2"
-                        />
+                    <input
+                        value={seller.price}
+                        onChange={(e) => onChange(index, "price", e.target.value)}
+                        placeholder="قیمت"
+                        type="number"
+                        className="w-1/4 h-9 text-sm rounded-md outline-none primary-border px-2"
+                    />
 
-                        {sellers.length > 1 &&
-                            <button
-                                type="button"
-                                className="text-red-500 shrink-0"
-                                onClick={() => onRemove(index)}
-                            >
-                                <HiX />
-                            </button>
-                        }
+                    <input
+                        value={seller.stock}
+                        onChange={(e) => onChange(index, "stock", e.target.value)}
+                        placeholder="موجودی"
+                        type="number"
+                        className="w-1/4 h-9 text-sm rounded-md outline-none primary-border px-2"
+                    />
+
+                    {sellers.length > 1 && (
+                        <button
+                            onClick={() => onRemove(index)}
+                            type="button"
+                            className="text-red-500 shrink-0"
+                        >
+                            <HiX />
+                        </button>
+                    )}
                 </div>
             ))}
-
-        </div >
+        </div>
     );
 };
 
